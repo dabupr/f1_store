3a87ca14d1e21ee3da76da78625f810122b5682b
David <dabupr@outlook.com>
2022-08-01 00:14:25 +0200

    ---

--- lib/infoPrductScreen/components/body_info.dart
+++ lib/infoPrductScreen/components/body_info.dart
@@ -7,93 +7,85 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Container(
-            height: 350,
-            width: MediaQuery.of(context).size.width,
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.elliptical(100, 100),
-                bottomRight: Radius.elliptical(100, 99),
-              ),
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Text(product.title,
-                    style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-                Expanded(child: Image(image: Image.asset(product.imageP).image))
-              ],
-
-//Image.asset(product.image).image,
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      //mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Container(
+          height: 350,
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.elliptical(100, 100),
+              bottomRight: Radius.elliptical(100, 99),
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(product.description,
-                            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              const SizedBox(height: 100),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              Text(product.title,
+                  style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+              Expanded(child: Image(image: Image.asset(product.imageP).image))
+            ],
+          ),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: ElevatedButton(
-
-                      onPressed: () {
-                        //debugPrint('ElevatedButton Clicked');
-                      },
-                      child: Text('Buy now'),
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.amber,
-                        minimumSize: Size(250, 50),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      //debugPrint('ElevatedButton Clicked');
-                    },
-                    child: Icon(
-                      Icons.shopping_cart,
-                      size: 20,
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      elevation: 0,
-                      primary: Colors.blueAccent[100],
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(product.description,
+                          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
                     ),
                   )
                 ],
               ),
-            ],
-          ),
-        ],
-      ),
+            ),
+             //Flexible(child:  SizedBox(height: 100)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.amber,
+                      minimumSize: const Size(250, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: const Text('Buy now'),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    primary: Colors.blueAccent[100],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: const Icon(
+                    Icons.shopping_cart,
+                    size: 20,
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
